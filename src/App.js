78655947d72ec0362384c3dc29d9ed8666a72dcb
ltasker443 <107644753+ltasker443@users.noreply.github.com>
2022-08-18 19:36:49 -0400

    removed unnecessary files and linked to Github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <a href="https://github.com/ltasker443/react-weather-app" rel="noreferrer" target="_blank"> Open Source by Laura Tasker </a>
     </div>
   );
 }
 
-export default App;
