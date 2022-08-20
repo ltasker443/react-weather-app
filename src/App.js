2f09f2953c30016182ec42e5f83a8d8b5c37ab69
ltasker443 <107644753+ltasker443@users.noreply.github.com>
2022-08-20 12:05:58 -0400

    added weather.js compnent

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import './App.css';
+import Weather from "./Weather.js"
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
+        <Weather />
       <a href="https://github.com/ltasker443/react-weather-app" rel="noreferrer" target="_blank"> Open Source by Laura Tasker </a>
       </div>
     </div>
