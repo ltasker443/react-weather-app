b5741e444abd9f947888f47f94d0b190d836d3ca
ltasker443 <107644753+ltasker443@users.noreply.github.com>
2022-08-20 11:59:52 -0400

    imported bootstrap

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <a href="https://github.com/ltasker443/react-weather-app" rel="noreferrer" target="_blank"> Open Source by Laura Tasker </a>
+      </div>
     </div>
   );
 }
