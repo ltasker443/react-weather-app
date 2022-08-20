593d2d1f8c471916c52e03d75985de943cbfe235
ltasker443 <107644753+ltasker443@users.noreply.github.com>
2022-08-20 12:29:40 -0400

    added HTML structure to Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,9 +1,37 @@
 import React from "react";
 
+
 export default function Weather() {
     return (
     <div className="Weather">
-        Hello from Weather
+        <input type="search" placeholder="City" />
+        <button type="submit" value="Search">
+            Search
+        </button>
+        <div class="card">
+            <div class="card-body">
+            <h3>Las Vegas </h3>
+            <h5>Clear</h5>
+            <div className="row">
+                <div className="col-6">
+                    103º
+                </div>
+                <div className="col-6">
+                    <ul>
+                        <li>
+                            Humidity: 54%
+                        </li>
+                        <li>
+                            Wind Speed: 4mph
+                        </li>
+                        <li>
+                            Feels Like: 104º
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            </div>
+        </div>
     </div>
     )
 }
