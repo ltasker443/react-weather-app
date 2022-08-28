5df819c0e46345c8a0285efa5b8d991847a8c882
ltasker443 <107644753+ltasker443@users.noreply.github.com>
2022-08-27 23:05:17 -0400

    added useEffect to update daily forecast

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import ForecastDay from "./ForecastDay.js"
 import "./Forecast.css";
@@ -7,6 +7,11 @@ import "./Forecast.css";
 export default function Forecast(props) {
     let[loaded, setLoaded] = useState(false);
     let[forecastData, setForecastData] = useState(null);
+
+    useEffect(() => {
+        setLoaded(false);
+    }, [props.coordinates]);
+    
 
     function handleResponse(response) {
         setForecastData(response.data.daily);
